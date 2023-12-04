bce438c6f9ca78f322f5ad2f7fcf427268316bde
Benedikt Schmidt <benedikt.schmidt@embedded-solutions.at>
2023-12-04 11:15:52 +0100

    group components in BOM by order number

--- scripts/bom_csv.py
+++ scripts/bom_csv.py
@@ -21,11 +21,6 @@ import kicad_utils
 import csv
 import sys
 
-# A helper function to filter/convert a string read in netlist
-#currently: do nothing
-def fromNetlistText( aText ):
-    return aText
-
 # Generate an instance of a generic netlist, and load the netlist tree from
 # the command line option. If the file doesn't exist, execution will stop
 net = kicad_netlist_reader.netlist(sys.argv[1])
@@ -53,24 +48,54 @@ out.writerow(['Ref', 'Qnty', 'Value', 'Cmp name', 'Footprint', 'Description', 'R
 
 # Get all of the components in groups of matching parts + values
 # (see ky_generic_netlist_reader.py)
-grouped = net.groupComponents()
+groupedComponents = net.groupComponents()
 
-# Output all of the component information
-for group in grouped:
-    refs = ""
+class Component():
+    def __init__(self, reference, value, partName, footprint, description, orderNumber):
+        self.reference = reference
+        self.value = value
+        self.partName = partName
+        self.footprint = footprint
+        self.description = description
+        self.orderNumber = orderNumber
 
-    # Add the reference of every component in the group and keep a reference
-    # to the component so that the other data can be filled in once per group
+    def __str__(self):
+        return f"{self.reference}: {self.value}, {self.orderNumber}"
+
+
+components = []
+
+for group in groupedComponents:
     for component in group:
-        refs += fromNetlistText( component.getRef() ) + ", "
-        c = component
+        convertedComponent = Component(
+            reference=component.getRef(),
+            value=component.getValue(),
+            partName=component.getPartName(),
+            footprint=component.getFootprint(),
+            description=component.getDescription(),
+            orderNumber=component.getField("RS order number"))
+        components.append(convertedComponent)
+        
+componentsByOrderNumber = {}
 
-    # Fill in the component groups common data
-    out.writerow([refs, len(group),
-        fromNetlistText( c.getValue() ),
-        fromNetlistText( c.getPartName() ),
-        fromNetlistText( c.getFootprint() ),
-        fromNetlistText( c.getDescription() ),
-        fromNetlistText( c.getField("RS order number") )])
+for component in components:
+    if component.orderNumber in componentsByOrderNumber:
+        componentsByOrderNumber[component.orderNumber].append(component)
+    else:
+        componentsByOrderNumber[component.orderNumber] = [component]
 
 
+for componentsList in componentsByOrderNumber.values():
+    seperator = ", "
+    references = seperator.join([component.reference for component in componentsList])
+    values = seperator.join([component.value for component in componentsList])
+    component = componentsList[0]
+
+    out.writerow([
+        references,
+        len(componentsList),
+        values,
+        component.partName,
+        component.footprint,
+        component.description,
+        component.orderNumber])
